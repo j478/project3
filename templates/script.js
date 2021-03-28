68555b9023f1426136aa112e01fd41a06d7377c8
Becky Lin <beckylin123@ku.edu>
2021-03-27 23:38:12 -0500

    Revert "Merge branch 'main' of https://github.com/j478/project3 into main"
    This reverts commit 536fea74a33141eb5e041228a99c5cd964357fd5, reversing
    changes made to bdd5c19ad8ea858b571abf8828d539fddcf23b2f.

--- templates/script.js
+++
@@ -1,6 +0,0 @@
-let file = "";
-
-document.querySelector('#add_file').addEventListener('change', () => {
-	file = document.querySelector('#add_file').files[0];
-	document.querySelector('#image').src = URL.createObjectURL(file);
-});
