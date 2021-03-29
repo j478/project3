f2b1f4e68e51bc6699beb8cefc168096106f8d8d
Alex Gisi <apgisi11@gmail.com>
2021-03-29 17:55:37 -0500

    Add /process endpoint

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from ocr import ocr
 
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static',
             static_url_path='/static')
+
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 @app.route('/')
@@ -22,7 +24,31 @@ def process_image():
     Process a file upload to our server.
     :return: JSON data resulting from our processing.
     """
-    pass
+    if 'file' not in request.files:
+        return jsonify(success=False,
+                       error_msg='No file in request.',
+                       img_text="")
+
+    file = request.files['file']
+    if file_good(file):
+        img_text = ocr(file.filename)
+
+        return jsonify(success=True,
+                       error_msg="",
+                       img_text=img_text)
+    else:
+        return jsonify(success=False,
+                       error_msg="File was not an image file/wrong filetype.",
+                       img_text="")
+
+
+def file_good(file):
+    if file.filename == '':
+        return False
+    elif file.filename not in ALLOWED_EXTENSIONS:
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
