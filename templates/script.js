67c507211e42275108454bd60795d078fb742ff0
j478 <jakedavies11@ku.edu>
2021-03-31 09:29:27 -0500

    added submit button

--- templates/script.js
+++ templates/script.js
@@ -4,3 +4,13 @@ document.querySelector('#add_file').addEventListener('change', () => {
 	file = document.querySelector('#add_file').files[0];
 	document.querySelector('#image').src = URL.createObjectURL(file);
 });
+
+/*document.querySelector('#submit').addEventListener('click', () => {
+	let form_data = new FormData(file);
+	$.ajax({
+		type: "POST",
+		url: "/process",
+		data: form_data,
+		cache: false,
+	});
+});*/
