882b35e7d590f004377f52dd516221c4502c5967
alexcjones <79162834+alexcjones@users.noreply.github.com>
2021-04-02 14:43:48 -0500

    declared xmlhttp request object
    makes the object, calls its function, reserved space to pull information out(line 23)

--- templates/script.js
+++ templates/script.js
@@ -5,12 +5,24 @@ document.querySelector('#add_file').addEventListener('change', () => {
 	document.querySelector('#image').src = URL.createObjectURL(file);
 });
 
-/*document.querySelector('#submit').addEventListener('click', () => {
-	let form_data = new FormData(file);
+
+document.querySelector('#submit').addEventListener('click', () => {
+	let form_data = new FormData(file)
+	/*
 	$.ajax({
 		type: "POST",
 		url: "/process",
 		data: form_data,
 		cache: false,
 	});
-});*/
+	*/
+	
+    let xttp=new XMLHttpRequest();
+    xttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          //this is where we pull the information out.
+        }
+      };
+      xhttp.open("GET", url, true);
+      xhttp.send();
+});
