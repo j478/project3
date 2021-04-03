e4b8c0d3e48d1899f51aed290642144080cceff2
Becky Lin <beckylin123@ku.edu>
2021-04-03 00:46:41 -0500

    enhance recognition

--- ocr.py
+++ ocr.py
@@ -7,17 +7,17 @@ TESSERACT_PATH = 'TESSDATA_PREFIX'
 def ocr(filename):
     img = cv2.imread(filename)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-    ret, thresh2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV)
+    thresh =cv2.threshold(gray,100,255,cv2.THRESH_OTSU|cv2.THRESH_BINARY)[1]
     kernel = np.ones((5, 5), np.uint8)
-    dilation = cv2.dilate(thresh1,thresh2, kernel, iterations=1)
-    contours, _git = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    contours, _git = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     text = ""
     for p in contours:
         x, y, w, h = cv2.boundingRect(p)
         rect = cv2.rectangle(img, (x, y), (x + w, y + h), 0, 2)
-        text = pytesseract.image_to_string(rect)
+        crop=img[y:y+h,x:x+w]
+        text = pytesseract.image_to_string(crop)
 
     return text
 
